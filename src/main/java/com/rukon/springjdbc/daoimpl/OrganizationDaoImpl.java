fc087ce913daff901ef334d1ffb552f7fb174122
Khan <khan2r@cmich.edu>
2021-04-24 02:39:43 -0400

    core project

---
+++ src/main/java/com/rukon/springjdbc/daoimpl/OrganizationDaoImpl.java
@@ -0,0 +1,54 @@
+package com.rukon.springjdbc.daoimpl;
+
+import com.rukon.springjdbc.dao.OrganizationDao;
+import com.rukon.springjdbc.domain.Organization;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Repository("orgDao")
+public class OrganizationDaoImpl implements OrganizationDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource datasource) {
+        jdbcTemplate = new JdbcTemplate(datasource);
+
+    }
+
+    public boolean create(Organization org) {
+        String sqlQuery = "INSERT INTO organization (compnay_name, year_of_incorporation, postal_code, employee_count, slogan)"
+        + "VALUES(?, ?, ?, ?, ?)";
+        Object args = new Object[] {org.getCompanyName(), org.getYearOfIncorporation(), org.getPostalCode(), org.getEmployeeCount(),
+                org.getSlogan()};
+        return jdbcTemplate.update(sqlQuery, args) == 1;
+    }
+
+    public Organization getOrganization(Integer id) {
+        return null;
+    }
+
+    public List<Organization> getAllOrganization() {
+        String sqlQuery = "SELECT * FROM organization";
+        List<Organization> orgList = jdbcTemplate.query(sqlQuery, new OrganizationRowMapper());
+
+        return orgList;
+    }
+
+    public boolean delete(Organization org) {
+        return false;
+    }
+
+    public boolean update(Organization org) {
+        return false;
+    }
+
+    public void cleanup() {
+        String sqlQuery = "TRUNCATE TABLE organization";
+        jdbcTemplate.execute(sqlQuery);
+    }
+}
---
+++ src/main/java/com/rukon/springjdbc/domain/Organization.java
@@ -0,0 +1,82 @@
+package com.rukon.springjdbc.domain;
+
+
+public class Organization {
+private int id;
+private String companyName;
+private int postalCode;
+private int yearOfIncorporation;
+private int employeeCount;
+private String slogan;
+
+    public Organization(String companyName, int postalCode, int yearOfIncorporation, int employeeCount, String slogan) {
+        this.companyName = companyName;
+        this.postalCode = postalCode;
+        this.yearOfIncorporation = yearOfIncorporation;
+        this.employeeCount = employeeCount;
+        this.slogan = slogan;
+    }
+
+    public Organization() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getYearOfIncorporation() {
+        return yearOfIncorporation;
+    }
+
+    public void setYearOfIncorporation(int yearOfIncorporation) {
+        this.yearOfIncorporation = yearOfIncorporation;
+    }
+
+    public int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(int employeeCount) {
+        this.employeeCount = employeeCount;
+    }
+
+    public String getSlogan() {
+        return slogan;
+    }
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+    }
+
+    @Override
+    public String toString() {
+        return "Organization{" +
+                "id=" + id +
+                ", companyName='" + companyName + '\'' +
+                ", postalCode=" + postalCode +
+                ", yearOfIncorporation=" + yearOfIncorporation +
+                ", employeeCount=" + employeeCount +
+                ", slogan='" + slogan + '\'' +
+                '}';
+    }
+}
